74a1b931ec75d60f9a7a950a35891859f2c37b3b
JPO <jeremiahoconnell@asus.jpocl.com>
2025-04-02 12:12:49 -0500

    20240402

--- src/building_blocks/infrastructure/BuildingBlocks.Infrastructure/obj/Debug/net9.0/BuildingBlocks.Infrastructure.AssemblyInfo.cs
+++ src/building_blocks/infrastructure/BuildingBlocks.Infrastructure/obj/Debug/net9.0/BuildingBlocks.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildingBlocks.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+855504e419eee0b53a0e3cc890525d747874f027")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af689c3b5e312f6e01bc8e91aa10f8fb0418934d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildingBlocks.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildingBlocks.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
